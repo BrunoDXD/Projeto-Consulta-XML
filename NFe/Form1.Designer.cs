58ccd73c74e84dee8b69df6140dd8879b51357d0
Bruno Silva <brunodxd09@gmail.com>
2024-09-25 08:31:35 -0300

    Projeto Finalizado

---
+++ NFe/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace NFe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(288, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 106);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Procurar Pasta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(700, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selecione a Pasta que deseja Extrair os Dados";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArquivo.Location = new System.Drawing.Point(47, 188);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.Size = new System.Drawing.Size(655, 34);
+            this.txtArquivo.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtArquivo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtArquivo;
+    }
+}
+
---
+++ NFe/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Xml;
+using Aspose.Cells;
+using System.Numerics;
+using static NFe.nfeProc;
+using Aspose.Cells.Tables;
+
+namespace NFe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string caminho;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Instancia o Objeto da Classe
+            nfeProc nota = new nfeProc();
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            txtArquivo.Text = fbd.SelectedPath.ToString();
+
+            //Marca o diretório a ser listado
+            DirectoryInfo diretorio = new DirectoryInfo(txtArquivo.Text);
+            //Executa função GetFile(Lista os arquivos desejados de acordo com o parametro)
+            FileInfo[] Arquivos = diretorio.GetFiles("*.xml;");
+
+            string[] files = System.IO.Directory.GetFiles(fbd.SelectedPath);
+
+            // Instanciar um objeto Workbook que representa o arquivo do Excel.
+            Workbook wb = new Workbook();
+
+
+            
+
+            for (int i = 2; i < files.Length+2; i++)
+            {
+                //Serializa o objeto
+                XmlSerializer ser = new XmlSerializer(typeof(nfeProc));
+
+                //lê o arquivo xml
+                TextReader textReader = (TextReader)new StreamReader(files[i-2]);
+                XmlTextReader reader = new XmlTextReader(textReader);
+                reader.Read();
+
+                //Desserializa o objeto
+                nota = (nfeProc)ser.Deserialize(reader);              
+
+
+                // Pegue a primeira planilha.
+                Worksheet sheet = wb.Worksheets[0];
+
+                // Obtendo a coleção de células da planilha
+                Cells cells = sheet.Cells;
+
+                cells["A1"].PutValue("Versão da Nota");
+                cells["A" + i].PutValue(Convert.ToString(nota.versao));
+
+                cells["B1"].PutValue("Código da NF");
+                cells["B" + i].PutValue(Convert.ToString(nota.NFe.infNFe.ide.cNF));
+
+                cells["C1"].PutValue("Nome do Emissor");
+                cells["C" + i].PutValue(Convert.ToString(nota.NFe.infNFe.emit.xNome));
+
+                cells["D1"].PutValue("CNPJ do Emissor");
+                cells["D" + i].PutValue(Convert.ToString(nota.NFe.infNFe.emit.CNPJ));
+
+                cells["E1"].PutValue("Data de Emissão");
+                cells["E" + i].PutValue(Convert.ToString(nota.NFe.infNFe.ide.dhEmi));
+
+                cells["F1"].PutValue("Data de Vencimento");
+                cells["F" + i].PutValue(Convert.ToString(nota.NFe.infNFe.cobr.dup.dVenc));
+
+            }
+
+            // Salve o arquivo Excel.
+            wb.Save("Excel_Table.xlsx", SaveFormat.Xlsx);
+
+
+        }
+
+
+    }
+}
